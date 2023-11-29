15473026671d647cc17f8e0a6d036bc64409df04
BoyanMark <bobsmarkov81@gmail.com>
2023-11-29 22:57:27 +0100

    Moved the questions to a separate questions.toml file.

--- Quiz/newquiz.py
+++ Quiz/newquiz.py
@@ -1,56 +1,13 @@
 # quiz.py
+import tomllib
+import pathlib
 import random
 from string import ascii_lowercase
 
-NUMBER_OF_QUESTIONS_ASKED = 9
-QUESTIONS = {
-    "What's one effect of calling random.seed(42)": [
-        "The random numbers are reproducible.",
-        "The random numbers are more random.",
-        "The computer clock is reset.",
-        "The first random number is always 42.",
-    ],
-    "When does __name__ == '__main__' equal True in a Python file": [
-        "When the file is run as a script",
-        "When the file is imported as a module",
-        "When the file has a valid name",
-        "When the file only has one function",
-    ],
-    "What's the purpose of the built-in zip() function": [
-        "To iterate over two or more sequences at the same time",
-        "To combine several strings into one",
-        "To compress several files into one archive",
-        "To get information from the user",
-    ],
-    "What's the name of Python's sorting algorithm": [
-        "Timsort", "Quicksort", "Merge sort", "Bubble sort"
-    ],
-    "What's the official name of the := operator": [
-        "Assignment expression",
-        "Named expression",
-        "Walrus operator",
-        "Colon equals operator",
-    ],
-    "What does dict.get(key) return if key isn't found in dict": [
-        "None", "key", "True", "False",
-    ],
-    "Which keyword do you use to loop over a given list of elements": [
-        "for", "while", "each", "loop"
-    ],
-    "Which built-in function can get information from the user": [
-        "input", "get", "print", "write"
-    ],
-    "When was the first known use of the word 'quiz'": [
-        "1781", "1771", "1871", "1881"
-    ],
-    "How do you iterate over both indices and elements in an iterable": [
-        "enumerate(iterable)",
-        "enumerate(iterable, start=1)",
-        "range(iterable)",
-        "range(iterable, start=1)",
-    ],
 
-}
+NUMBER_OF_QUESTIONS_ASKED = 9
+QUESTIONS_PATH = pathlib.Path(__file__).parent / "questions.toml"
+QUESTIONS = tomllib.loads(QUESTIONS_PATH.read_text())
 
 
 def prepare_questions(questions, number_of_questions):
